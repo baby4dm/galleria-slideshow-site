a0affd15b1266a56aa992dc23858f408e7de1243
baby4dm <babych.dmtr@gmail.com>
2025-12-08 17:42:54 +0200

    added basic layuot

--- src/main.js
+++ src/main.js
@@ -7,21 +7,23 @@ async function loadData() {
 function renderGallery(items) {
   const gallery = document.getElementById("gallery");
 
-  items.forEach((item) => {
-    gallery.innerHTML += `
-      <div class="relative group cursor-pointer">
-        <img 
-          src="${item.images.thumbnail}" 
-          alt="${item.name}"
-          class="w-full h-auto block"
-        />
-        <div class="absolute bottom-4 left-4 text-white">
-          <h2 class="text-lg font-bold">${item.name}</h2>
-          <p class="text-sm opacity-70">${item.artist.name}</p>
-        </div>
+  gallery.innerHTML = items
+    .map(
+      (item) => `
+    <div class="relative cursor-pointer">
+      <img
+        src="${item.images.thumbnail}"
+        alt="${item.name}"
+        class="w-full h-auto block"
+      />
+      <div class="absolute bottom-4 left-4 text-white">
+        <h2 class="text-lg font-bold">${item.name}</h2>
+        <p class="text-sm opacity-70">${item.artist.name}</p>
       </div>
-    `;
-  });
+    </div>
+  `
+    )
+    .join("");
 }
 
 loadData();
