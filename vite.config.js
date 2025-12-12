8f8e5d609b2df4f89eb55e4a5bde4f22235fd041
baby4dm <babych.dmtr@gmail.com>
2025-12-12 10:09:44 +0200

    added deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import htmlInject from "vite-plugin-html-inject";
 
 export default defineConfig({
+  base: "/galleria-slideshow-site/",
   plugins: [
     tailwindcss(),
     htmlInject({
